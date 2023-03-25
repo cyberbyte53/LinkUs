c4e3970ae583816a5ca30e3dfc79dc12389275c0
Naman Singh Rana <nsr@Namans-MacBook-Air.local>
2023-03-25 23:44:58 +0530

    restrut

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require("express");
-require('dotenv').config({path:'./config.env'});
+// const express = require("express");
+// require('dotenv').config({path:'./config.env'});
       
-const app = express();
+// const app = express();
 
-// set port, listen for requests
-const PORT = process.env.BACKEND_PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+// // set port, listen for requests
+// const PORT = process.env.BACKEND_PORT || 8080;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
 
-app.get('/hello_world',(req,res) => {
-  return res.json({ "Hello": "World" });
-});
+// app.get('/hello_world',(req,res) => {
+//   return res.json({ "Hello": "World" });
+// });
 
+
+// "dotenv": "^16.0.3",
+// "express": "^4.18.2"
